7bb8d1fadb26711c3e73acdb69be71d3a8cc6cf3
Aparna Menon S <aparna.menon@diggibyte.com>
2023-04-21 12:39:13 +0000

    Prod_revenue Updated.

--- models/kpis/prod_revenue_year.sql
+++ models/kpis/prod_revenue_year.sql
@@ -5,5 +5,4 @@ select
 from {{ ref('stg_order') }} orders
 inner join {{ ref('stg_orderitem') }} items on orders.order_id = items.order_id
 group by items.product_id, year
-having year = 2023
 order by items.product_id
