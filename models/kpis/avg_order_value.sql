191279c143d904e6b0f3af72e209b232dc19ac7c
Aparna Menon S <aparna.menon@diggibyte.com>
2023-05-08 05:00:04 +0000

    Updated avg_order_value

--- models/kpis/avg_order_value.sql
+++ models/kpis/avg_order_value.sql
@@ -2,4 +2,4 @@ select orders.order_id, round(avg(items.total_amount), 2) as avg_order_value
 from {{ ref("stg_order") }} orders
 inner join {{ ref("stg_orderitem") }} items on orders.order_id = items.order_id
 group by orders.order_id
-order by avg_order_value desc
+order by order_id asc
